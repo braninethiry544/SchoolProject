48abfb516d3b4fbe5d7bf60f1c4bda6467e31a41
braninethiry544 <braninethiry544@gmail.com>
2025-03-08 21:32:14 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-schoolproject',
+  templateUrl: './schoolproject.component.html',
+  styleUrls: ['./schoolproject.component.css']
+})
+export class SchoolprojectComponent {
+}
